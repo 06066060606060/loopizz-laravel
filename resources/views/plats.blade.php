a71633fa9e354b8169537f8974cd158c376be71a
06066060606060 <xmicky+git@hotmail.fr>
2023-02-18 14:57:56 +0400

    update

--- resources/views/plats.blade.php
+++ resources/views/plats.blade.php
@@ -1,6 +1,140 @@
  @extends('layouts.app')
 
  @section('main')
-<div class="h-96"></div>
+     <section class="pt-8">
 
+         <div class="p-8 md:p-12 lg:px-12 lg:py-12">
+             <div class="max-w-lg mx-auto text-center lg:max-w-xl">
+                 <h2 class="text-3xl font-bold text-gray-900 md:text-5xl">
+                     Recherche
+                 </h2>
+
+             </div>
+
+             <div class="max-w-2xl mx-auto my-8">
+                 <div action=" " class="sm:flex sm:gap-4">
+                     <div class="sm:flex-1">
+                         <input id="searchbar" type="text" placeholder="SEARCH" name="q" value=""
+                             class="w-full p-3 text-gray-700 transition bg-gray-100 border-gray-200 rounded-md shadow-sm focus:border-white focus:outline-none focus:ring focus:ring-gray-400" />
+                     </div>
+                 </div>
+             </div>
+
+             <section class="text-gray-600 body-font">
+                 <div class="container px-5 py-24 mx-auto">
+                     <div class="flex flex-wrap -m-4">
+                         <div class="p-4 md:w-1/3">
+                             <div class="h-full overflow-hidden border-2 border-gray-200 rounded-lg border-opacity-60">
+                                 <img class="object-cover object-center w-full lg:h-48 md:h-36"
+                                     src="https://dummyimage.com/720x400" alt="blog">
+                                 <div class="p-6">
+                                     <h2 class="mb-1 text-xs font-medium tracking-widest text-gray-400 title-font">CATEGORY
+                                     </h2>
+                                     <h1 class="mb-3 text-lg font-medium text-gray-900 title-font">The Catalyzer</h1>
+                                     <p class="mb-3 leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings
+                                         jianbing microdosing tousled waistcoat.</p>
+                                     <div class="flex flex-wrap items-center ">
+                                         <a class="inline-flex items-center text-indigo-500 md:mb-2 lg:mb-0">Plus d'infos</a>
+                                        
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                         <div class="p-4 md:w-1/3">
+                             <div class="h-full overflow-hidden border-2 border-gray-200 rounded-lg border-opacity-60">
+                                 <img class="object-cover object-center w-full lg:h-48 md:h-36"
+                                     src="https://dummyimage.com/721x401" alt="blog">
+                                 <div class="p-6">
+                                     <h2 class="mb-1 text-xs font-medium tracking-widest text-gray-400 title-font">CATEGORY
+                                     </h2>
+                                     <h1 class="mb-3 text-lg font-medium text-gray-900 title-font">The 400 Blows</h1>
+                                     <p class="mb-3 leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings
+                                         jianbing microdosing tousled waistcoat.</p>
+                                     <div class="flex flex-wrap items-center">
+                                         <a class="inline-flex items-center text-indigo-500 md:mb-2 lg:mb-0">Plus d'infos</a>
+                                         
+                                        
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                         <div class="p-4 md:w-1/3">
+                             <div class="h-full overflow-hidden border-2 border-gray-200 rounded-lg border-opacity-60">
+                                 <img class="object-cover object-center w-full lg:h-48 md:h-36"
+                                     src="https://dummyimage.com/722x402" alt="blog">
+                                 <div class="p-6">
+                                     <h2 class="mb-1 text-xs font-medium tracking-widest text-gray-400 title-font">CATEGORY
+                                     </h2>
+                                     <h1 class="mb-3 text-lg font-medium text-gray-900 title-font">Shooting Stars</h1>
+                                     <p class="mb-3 leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings
+                                         jianbing microdosing tousled waistcoat.</p>
+                                     <div class="flex flex-wrap items-center ">
+                                         <a class="inline-flex items-center text-indigo-500 md:mb-2 lg:mb-0">Plus d'infos</a>
+                                         
+                                        
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                         <div class="p-4 md:w-1/3">
+                             <div class="h-full overflow-hidden border-2 border-gray-200 rounded-lg border-opacity-60">
+                                 <img class="object-cover object-center w-full lg:h-48 md:h-36"
+                                     src="https://dummyimage.com/722x402" alt="blog">
+                                 <div class="p-6">
+                                     <h2 class="mb-1 text-xs font-medium tracking-widest text-gray-400 title-font">CATEGORY
+                                     </h2>
+                                     <h1 class="mb-3 text-lg font-medium text-gray-900 title-font">Shooting Stars</h1>
+                                     <p class="mb-3 leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings
+                                         jianbing microdosing tousled waistcoat.</p>
+                                     <div class="flex flex-wrap items-center ">
+                                         <a class="inline-flex items-center text-indigo-500 md:mb-2 lg:mb-0">Plus d'infos</a>
+                                         
+                                        
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                         <div class="p-4 md:w-1/3">
+                             <div class="h-full overflow-hidden border-2 border-gray-200 rounded-lg border-opacity-60">
+                                 <img class="object-cover object-center w-full lg:h-48 md:h-36"
+                                     src="https://dummyimage.com/722x402" alt="blog">
+                                 <div class="p-6">
+                                     <h2 class="mb-1 text-xs font-medium tracking-widest text-gray-400 title-font">CATEGORY
+                                     </h2>
+                                     <h1 class="mb-3 text-lg font-medium text-gray-900 title-font">Shooting Stars</h1>
+                                     <p class="mb-3 leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings
+                                         jianbing microdosing tousled waistcoat.</p>
+                                     <div class="flex flex-wrap items-center ">
+                                         <a class="inline-flex items-center text-indigo-500 md:mb-2 lg:mb-0">Plus d'infos</a>
+                                         
+                                        
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                         <div class="p-4 md:w-1/3">
+                             <div class="h-full overflow-hidden border-2 border-gray-200 rounded-lg border-opacity-60">
+                                 <img class="object-cover object-center w-full lg:h-48 md:h-36"
+                                     src="https://dummyimage.com/722x402" alt="blog">
+                                 <div class="p-6">
+                                     <h2 class="mb-1 text-xs font-medium tracking-widest text-gray-400 title-font">CATEGORY
+                                     </h2>
+                                     <h1 class="mb-3 text-lg font-medium text-gray-900 title-font">Shooting Stars</h1>
+                                     <p class="mb-3 leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings
+                                         jianbing microdosing tousled waistcoat.</p>
+                                     <div class="flex flex-wrap items-center ">
+                                         <a class="inline-flex items-center text-indigo-500 md:mb-2 lg:mb-0">Plus d'infos</a>
+                                         
+                                        
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </section>
+
+
+         </div>
+     </section>
  @endsection
--- resources/views/professionnel.blade.php
+++ resources/views/professionnel.blade.php
@@ -1,6 +1,99 @@
  @extends('layouts.app')
 
  @section('main')
-
-<div class="h-96"></div>
+     <section class="text-gray-600 body-font">
+         <div class="container px-5 py-24 mx-auto">
+             <div class="flex flex-wrap w-full mb-20">
+                 <div class="w-full mb-6 lg:w-1/2 lg:mb-0">
+                     <h1 class="mb-2 text-2xl font-medium text-gray-900 sm:text-3xl title-font">Liste des professionnels</h1>
+                     <div class="h-1 bg-blue-500 rounded w-80"></div>
+                 </div>
+                 <p class="w-full leading-relaxed text-gray-500 lg:w-1/2">Whatever cardigan tote bag tumblr hexagon brooklyn
+                     asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man
+                     bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+             </div>
+             <div class="flex flex-wrap -m-4">
+                 <div class="p-4 xl:w-1/4 md:w-1/2">
+                     <div class="p-6 bg-gray-100 rounded-lg">
+                         <img class="object-cover object-center w-full h-40 mb-6 rounded"
+                             src="https://dummyimage.com/720x400" alt="content">
+                         <h3 class="text-xs font-medium tracking-widest text-indigo-500 title-font">SUBTITLE</h3>
+                         <h2 class="mb-4 text-lg font-medium text-gray-900 title-font">Chichen Itza</h2>
+                         <p class="text-base leading-relaxed">Fingerstache flexitarian street art 8-bit waistcoat.
+                             Distillery hexagon disrupt edison bulbche.</p>
+                     </div>
+                 </div>
+                 <div class="p-4 xl:w-1/4 md:w-1/2">
+                     <div class="p-6 bg-gray-100 rounded-lg">
+                         <img class="object-cover object-center w-full h-40 mb-6 rounded"
+                             src="https://dummyimage.com/721x401" alt="content">
+                         <h3 class="text-xs font-medium tracking-widest text-indigo-500 title-font">SUBTITLE</h3>
+                         <h2 class="mb-4 text-lg font-medium text-gray-900 title-font">Colosseum Roma</h2>
+                         <p class="text-base leading-relaxed">Fingerstache flexitarian street art 8-bit waistcoat.
+                             Distillery hexagon disrupt edison bulbche.</p>
+                     </div>
+                 </div>
+                 <div class="p-4 xl:w-1/4 md:w-1/2">
+                     <div class="p-6 bg-gray-100 rounded-lg">
+                         <img class="object-cover object-center w-full h-40 mb-6 rounded"
+                             src="https://dummyimage.com/722x402" alt="content">
+                         <h3 class="text-xs font-medium tracking-widest text-indigo-500 title-font">SUBTITLE</h3>
+                         <h2 class="mb-4 text-lg font-medium text-gray-900 title-font">Great Pyramid of Giza</h2>
+                         <p class="text-base leading-relaxed">Fingerstache flexitarian street art 8-bit waistcoat.
+                             Distillery hexagon disrupt edison bulbche.</p>
+                     </div>
+                 </div>
+                 <div class="p-4 xl:w-1/4 md:w-1/2">
+                     <div class="p-6 bg-gray-100 rounded-lg">
+                         <img class="object-cover object-center w-full h-40 mb-6 rounded"
+                             src="https://dummyimage.com/723x403" alt="content">
+                         <h3 class="text-xs font-medium tracking-widest text-indigo-500 title-font">SUBTITLE</h3>
+                         <h2 class="mb-4 text-lg font-medium text-gray-900 title-font">San Francisco</h2>
+                         <p class="text-base leading-relaxed">Fingerstache flexitarian street art 8-bit waistcoat.
+                             Distillery hexagon disrupt edison bulbche.</p>
+                     </div>
+                 </div>
+                 <div class="p-4 xl:w-1/4 md:w-1/2">
+                     <div class="p-6 bg-gray-100 rounded-lg">
+                         <img class="object-cover object-center w-full h-40 mb-6 rounded"
+                             src="https://dummyimage.com/723x403" alt="content">
+                         <h3 class="text-xs font-medium tracking-widest text-indigo-500 title-font">SUBTITLE</h3>
+                         <h2 class="mb-4 text-lg font-medium text-gray-900 title-font">San Francisco</h2>
+                         <p class="text-base leading-relaxed">Fingerstache flexitarian street art 8-bit waistcoat.
+                             Distillery hexagon disrupt edison bulbche.</p>
+                     </div>
+                 </div>
+                 <div class="p-4 xl:w-1/4 md:w-1/2">
+                     <div class="p-6 bg-gray-100 rounded-lg">
+                         <img class="object-cover object-center w-full h-40 mb-6 rounded"
+                             src="https://dummyimage.com/723x403" alt="content">
+                         <h3 class="text-xs font-medium tracking-widest text-indigo-500 title-font">SUBTITLE</h3>
+                         <h2 class="mb-4 text-lg font-medium text-gray-900 title-font">San Francisco</h2>
+                         <p class="text-base leading-relaxed">Fingerstache flexitarian street art 8-bit waistcoat.
+                             Distillery hexagon disrupt edison bulbche.</p>
+                     </div>
+                 </div>
+                 <div class="p-4 xl:w-1/4 md:w-1/2">
+                     <div class="p-6 bg-gray-100 rounded-lg">
+                         <img class="object-cover object-center w-full h-40 mb-6 rounded"
+                             src="https://dummyimage.com/723x403" alt="content">
+                         <h3 class="text-xs font-medium tracking-widest text-indigo-500 title-font">SUBTITLE</h3>
+                         <h2 class="mb-4 text-lg font-medium text-gray-900 title-font">San Francisco</h2>
+                         <p class="text-base leading-relaxed">Fingerstache flexitarian street art 8-bit waistcoat.
+                             Distillery hexagon disrupt edison bulbche.</p>
+                     </div>
+                 </div>
+                 <div class="p-4 xl:w-1/4 md:w-1/2">
+                     <div class="p-6 bg-gray-100 rounded-lg">
+                         <img class="object-cover object-center w-full h-40 mb-6 rounded"
+                             src="https://dummyimage.com/723x403" alt="content">
+                         <h3 class="text-xs font-medium tracking-widest text-indigo-500 title-font">SUBTITLE</h3>
+                         <h2 class="mb-4 text-lg font-medium text-gray-900 title-font">San Francisco</h2>
+                         <p class="text-base leading-relaxed">Fingerstache flexitarian street art 8-bit waistcoat.
+                             Distillery hexagon disrupt edison bulbche.</p>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </section>
  @endsection
